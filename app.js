4fc187f03ddd40aac5145ce25b10d527809d9652
Darbyblinko <jacobcrombie@live.com>
2020-08-21 17:25:38 -0600

    most of the functionality

--- app.js
+++ app.js
@@ -1,40 +1,72 @@
 //Elements
 
-let clickCount = 0
+
+let autoUpgradesElem = document.getElementById("autoUpgrades")
+let clickUpgradesElem = document.getElementById("clickUpgrades")
+let playerElem = document.getElementById("player")
 
 // Player Data
 
+
 let player = {
   name: "Jake",
-  upgrades:{},
-  clickCount: 0
+  count: 1000
 }
+
 
 //Upgrades Data
-
-let clickUpgrades = {
-  pickaxes: {
+let clickUpgrades = [
+  {
+    name: "Pickaxes",
     price: 100,
-    quantity: 0,
+    quantity: 1,
     multiplier: 1
   }
-}
 
-let automaticUpgrades = {
-  rovers: {
+
+]
+
+
+let autoUpgrades = [
+  {
+    name: "Rovers",
     price: 600,
     quantity: 0,
     multiplier: 20
   }
+
+]
+
+
+
+//#region GAME LOGIC
+
+function mine() {
+  // forloop to go over upgrades multiply quantity by multiplier
+    clickUpgrades.forEach(upgrade => {
+    player.count += upgrade.quantity * upgrade.multiplier +1
+  })
+
+  drawPlayer()
 }
 
-
-//#region 
-
-function mine(){
-  clickCount ++
-  console.log(clickCount)
-  drawClicks()
+function purchase(name) {
+  // if statement for (player.count >= upgrade.price) then increase upgrade quantity by 1 and decrease player.count by upgrade.price
+  
+  let upgrade = clickUpgrades.find(u => u.name == name)
+   if (!upgrade) {
+     upgrade = autoUpgrades.find(u => u.name == name)
+   }
+  
+  
+  if(upgrade.price> player.count){
+     return
+   }else{
+     upgrade.quantity++
+     player.count -= upgrade.price
+   }
+  drawPlayer()
+  drawUpgrades()
 }
 
 //#endregion
@@ -42,14 +74,36 @@ function mine(){
 
 //#region Draw Methods
 
-function drawClicks(){
-  document.getElementById("clickCount").innerText = clickCount.toString()
+function drawPlayer() {
+  playerElem.innerHTML = `<div>Player Name: ${player.name}  Clicks: ${player.count} </div>`
 }
+
+
+function drawUpgrades() {
+  let autotemplate = ""
+  let clicktemplate = ""
+
+  autoUpgrades.forEach(upgrade => {
+    autotemplate += upgradeTemplate(upgrade)
+  })
+
+  clickUpgrades.forEach(upgrade => {
+    clicktemplate += upgradeTemplate(upgrade)
+  })
+  autoUpgradesElem.innerHTML = autotemplate
+  clickUpgradesElem.innerHTML = clicktemplate
+}
+
+function upgradeTemplate(upgrade) {
+  return `<button class="" onclick="purchase('${upgrade.name}')">Type: ${upgrade.name} Price: ${upgrade.price} Quantity: ${upgrade.quantity} Multiplier: ${upgrade.multiplier}</button>`
+}
+
 
 //#endregion
 
-function updateScreen(){
-  drawClicks()
+function updateScreen() {
+  drawUpgrades()
+  drawPlayer()
 }
 
 updateScreen()
